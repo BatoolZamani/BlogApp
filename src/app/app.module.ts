46e0beeebdbddb37dc63ccd4d150806f5ed5af0b
b.zamani@renolos.com <b.zamani@renolos.com>
2024-12-25 16:05:46 +0100

    routing and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Layouts/header/header.component';
+import { CategoryNavbarComponent } from './Layouts/category-navbar/category-navbar.component';
+import { Routes, RouterModule } from '@angular/router';
+import { routes } from './app.routes';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+  
+  ],
+imports:[
+  BrowserModule,
+  RouterModule.forRoot(routes)
+],
+exports:[RouterModule],
+  providers:[],
+  bootstrap:[]
 })
+
 export class AppModule { }
