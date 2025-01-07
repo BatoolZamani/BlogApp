c650ca4fe89b331d43b5077fc01455f967f76eaf
b.zamani@renolos.com <b.zamani@renolos.com>
2025-01-07 11:06:27 +0100

    navbar complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./Layouts/header/header.component";
+import { CategoryNavbarComponent } from "./Layouts/category-navbar/category-navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, CategoryNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
