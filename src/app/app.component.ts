66bb71d6b77da4edf747c2efd433cd80af057ef7
b.zamani@renolos.com <b.zamani@renolos.com>
2025-01-08 10:51:48 +0100

    footer complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./Layouts/header/header.component";
 import { CategoryNavbarComponent } from "./Layouts/category-navbar/category-navbar.component";
+import { FooterComponent } from './Layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, CategoryNavbarComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, CategoryNavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
