1838fa5049172748d61e48d8b22df60ab7cb7978
b.zamani@renolos.com <b.zamani@renolos.com>
2025-01-08 11:32:56 +0100

    card complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./Layouts/header/header.component";
 import { CategoryNavbarComponent } from "./Layouts/category-navbar/category-navbar.component";
 import { FooterComponent } from './Layouts/footer/footer.component';
+import { PostCardComponent } from "./Layouts/post-card/post-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, CategoryNavbarComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent, PostCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
