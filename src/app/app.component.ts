db0f440c2d755c15ecb5bc1f4934a453c0c2790d
b.zamani@renolos.com <b.zamani@renolos.com>
2025-01-09 12:13:15 +0100

    subscribe-form design part1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from "./Layouts/header/header.component";
 import { CategoryNavbarComponent } from "./Layouts/category-navbar/category-navbar.component";
 import { FooterComponent } from './Layouts/footer/footer.component';
 import { PostCardComponent } from "./Layouts/post-card/post-card.component";
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent, PostCardComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent, PostCardComponent,SubscriptionFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
